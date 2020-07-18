f46f92c5b3bd259f058a3ea85e48217543d4f155
esamdaghreri <esam@daghreri.com>
2020-07-18 14:08:27 +0300

    create route and controller for update post

--- controllers/feedController.js
+++ controllers/feedController.js
@@ -1,6 +1,10 @@
 // Third party libraries
 const { validationResult } = require('express-validator/check');
 
+// node js library
+const path = require('path');
+const fs = require('fs');
+
 // Import models
 const Post = require('../models/post');
 
@@ -84,4 +88,58 @@ module.exports.getPost = (req, res, next) => {
       }
       next(error);
     });
+};
+
+module.exports.updatePost = (req, res, next) => {
+  const postId = req.params.postId;
+    // Get validation errors
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect.');
+    error.statusCode = 422;
+    throw error;
+  }
+
+  const title = req.body.title;
+  const content = req.body.content;
+  let imageUrl = req.body.image;
+
+  if(req.file){
+    imageUrl = req.file.path.replace("\\" ,"/");
+  }
+
+  if(!imageUrl){
+    const error = new Error('Image not uploaded!');
+    error.statusCode = 422;
+    throw error;
+  }
+  Post.findById(postId)
+    .then(post => {
+      if(!post) {
+        const error = new Error('Post not found');
+        error.statusCode = 404;
+        throw error;
+      }
+      if(imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+      post.title = title;
+      post.imageUrl = imageUrl;
+      post.content = content;
+      return post.save();
+    })
+    .then(post => {
+      res.status(200).json({message: 'Post updated!', post: post});
+    })
+    .catch(error => {
+      if(!error.statusCode){
+        error.statusCode = 500;
+      }
+      next(error);
+    });
+};
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, '..', filePath);
+  fs.unlink(filePath, error => console.log(error));
 };
